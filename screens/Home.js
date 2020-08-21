ab329e77786360d6508c5f418dd454680cf10430
derrick-lau <derrickalau@gmail.com>
2020-08-21 08:41:39 -0700

    create new activity

--- screens/Home.js
+++ screens/Home.js
@@ -34,31 +34,35 @@ class Home extends React.Component {
         <FlatList
           data={this.props.post.feed}
           keyExtractor={(item) => item.id}
-          renderItem={({item}) => (
-            <View>
-              <View style={[styles.row, styles.center]}>
+          renderItem={({item}) => {
+            const liked = item.likes.includes(this.props.user.uid)
+            return (
+              <View>
                 <View style={[styles.row, styles.center]}>
-                  <Image style={styles.roundImage} source={{uri: item.photo}}/>
-                  <View>
-                    <Text>{item.username}</Text>
-                    <TouchableOpacity onPress={() => this.navigateMap(item)} >
-                      <Text>{item.postLocation ? item.postLocation.name : null}</Text>
-                    </TouchableOpacity>
+                  <View style={[styles.row, styles.center]}>
+                    <Image style={styles.roundImage} source={{uri: item.photo}}/>
+                    <View>
+                      <Text>{item.username}</Text>
+                      <TouchableOpacity onPress={() => this.navigateMap(item)} >
+                        <Text>{item.postLocation ? item.postLocation.name : null}</Text>
+                      </TouchableOpacity>
+                    </View>
                   </View>
+                  <Ionicons style={{margin: 5}} name='ios-flag' size={25} />
                 </View>
-                <Ionicons style={{margin: 5}} name='ios-flag' size={25} />
+                <TouchableOpacity onPress={() => this.likePost(item)} >
+                  <Image style={styles.postPhoto} source={{uri: item.postPhoto}}/>
+                </TouchableOpacity>
+                <View style={styles.row}>
+                  <Ionicons style={{margin: 5}} color={liked ? '#db565b' : '#000'} name={ liked ? 'ios-heart' : 'ios-heart-empty'} size={25} />
+                  <Ionicons style={{margin: 5}} name='ios-chatbubbles' size={25} />
+                  <Ionicons style={{margin: 5}} name='ios-send' size={25} />
+                </View>
+                <Text>{item.postDescription}</Text>
               </View>
-              <TouchableOpacity onPress={() => this.likePost(item)} >
-                <Image style={styles.postPhoto} source={{uri: item.postPhoto}}/>
-              </TouchableOpacity>
-              <View style={styles.row}>
-                <Ionicons style={{margin: 5}} name={item.likes.includes(this.props.user.uid) ? 'ios-heart' : 'ios-heart-empty'} size={25} />
-                <Ionicons style={{margin: 5}} name='ios-chatbubbles' size={25} />
-                <Ionicons style={{margin: 5}} name='ios-send' size={25} />
-              </View>
-              <Text>{item.postDescription}</Text>
-            </View>
-          )}
+            )
+
+          }}
         />
       </View>
     );
